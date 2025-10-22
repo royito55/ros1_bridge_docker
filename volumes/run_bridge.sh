ccf83bfa73dbad872adc9b35aebd9eacb91504c8
Rodrigo <roalgoal@gmail.com>
2025-10-22 17:32:19 +0200

    Changes for FastBot RRL

--- volumes/run_bridge.sh
+++ volumes/run_bridge.sh
@@ -2,6 +2,7 @@
 
 source /opt/ros/noetic/setup.bash
 source /catkin_ws/devel/setup.bash
+roscore &
 roslaunch load_params load_params.launch --wait --skip-log-check
 source /opt/ros/galactic/setup.bash
 ros2 run ros1_bridge parameter_bridge
